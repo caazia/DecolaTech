c3e6d3ee998f4060ecc124fba23589c8f7c79c8f
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-11-29 17:55:21 -0300

    Add files via upload

---
+++ TiposOperadores/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using TiposOperadores.Models;
+// int quantidadeEmEstoque = 3;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda inválida.");
+// }
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada.");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+// }
+
+/*
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
+*/
+
+/*
+// OPERADOR OR
+bool ehMaiorDeIdade = true;
+bool possuiAutorizacaoDoResponsavel = false;
+
+if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+{
+    Console.WriteLine("Entrada liberada!");
+}
+else
+{
+    Console.WriteLine("Entrada não liberada");
+}
+
+*/
+/*
+// operador AND
+
+bool possuiPresencaMinima = false;
+double media = 7.5;
+
+if (possuiPresencaMinima && media >= 7)
+{
+    Console.WriteLine("Aprovado!");
+} 
+else
+{
+    Console.WriteLine("Reprovado");
+}
+*/
+/*
+
+// OPERADOR NOT
+
+bool choveu = false;
+bool estaTarde = false;
+    
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia");
+}
+
+*/
+
+
+
+//CALCULADORA
+
+using TiposOperadores.Models;
+Calculadora calc = new Calculadora();
+
+/*
+calc.Somar(10, 50);
+calc.Subtrair(10, 8);
+calc.Multiplicar(20, 50);
+calc.Dividir(50, 5);
+calc.Potencia(3, 3);
+
+*/
+
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9);
+
