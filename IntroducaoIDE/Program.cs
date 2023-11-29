1c89deb22a860340e9d189abf96b3f5dc7703d8c
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-11-29 04:32:26 -0300

    Add files via upload

---
+++ IntroducaoIDE/Program.cs
@@ -0,0 +1,34 @@
+﻿using IntroducaoIDE.Models;
+
+// Cast - Casting
+// int a = Convert.ToInt32("5");
+// int a = int.Parse("5");
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+/*
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Cássia";
+pessoa1.Idade = 29;
+pessoa1.Apresentar();
+
+*/
+
+// string apresentacao = "Olá, seja bem vindo!";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variavel preco: " + preco);
+// Console.WriteLine("Valor da variavel condição: " + condicao);
+
