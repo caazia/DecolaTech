f1bfc2f8a3f49ec1180dc4000f71e5651c2f4589
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-11-29 21:13:35 -0300

    Add files via upload

---
+++ EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,83 @@
+﻿using EstruturasDeRepeticao.Models;
+
+
+/*
+int numero = 10;
+int contador = 0;
+
+for(int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
+*/
+
+/*
+
+while (contador <= 10)
+{
+    Console.WriteLine($"{contador + 1}° Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+
+    if (contador == 6)
+    {
+        break;
+    }
+
+}
+
+*/
+
+/*
+
+int soma = 0, numero = 0;
+
+do
+{
+    Console.WriteLine("Digite um número (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+
+} while (numero != 0);
+
+Console.WriteLine($"Total da soma dos números dgitados é: {soma}");
+
+*/
+
+string opcao;
+bool exibirMenu = true;
+
+
+while(exibirMenu)
+{
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida, tente novamente.");
+            break;
+    }
+}
+
+Console.WriteLine("O programa se encerrou.");
