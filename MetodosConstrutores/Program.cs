fc5a4f1de876bd43416443388b037300d7dfecd2
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-12-01 19:46:35 -0300

    Add files via upload

---
+++ MetodosConstrutores/Program.cs
@@ -0,0 +1,21 @@
+﻿using MetodosConstrutores.Models;
+
+Pessoa p1 = new Pessoa(nome: "Cássia", sobrenome: "Carvalho");
+
+Pessoa p2 = new Pessoa(nome: "Danilo", sobrenome: "Acioly");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Cássia";
+// p1.Sobrenome = "Carvalho";
+// p1.Idade = 29;
+// p1.Apresentar();
