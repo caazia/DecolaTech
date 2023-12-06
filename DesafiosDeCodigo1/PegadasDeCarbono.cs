f10488e5ac337a43c2ec183ab540a787f638e786
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-12-06 19:59:39 -0300

    Add files via upload

---
+++ DesafiosDeCodigo1/PegadasDeCarbono.cs
@@ -0,0 +1,26 @@
+class Program
+{
+    static void Main()
+    {
+        // Solicita o nome do usuário, quilômetros percorridos por dia, 
+       // Horas de uso de eletrônicos por dia e o número de refeições com carne:
+       string nome = Console.ReadLine();
+       double quilometrosPorDia = double.Parse(Console.ReadLine());
+       int horasDeEletronicos = int.Parse(Console.ReadLine());
+       int refeicoesComCarne = int.Parse(Console.ReadLine());
+
+        // Chama o método para calcular a pegada de carbono
+        double pegadaDeCarbono = CalcularPegadaDeCarbono(quilometrosPorDia, horasDeEletronicos, refeicoesComCarne);
+        
+        Console.WriteLine($"{nome}, sua pegada de carbono e de {pegadaDeCarbono} toneladas de CO2 por ano." );
+
+        // Aguarda a entrada do usuário antes de encerrar o programa:
+        Console.ReadLine();
+    }
+
+  static double CalcularPegadaDeCarbono(double quilometrosPorDia, int horasDeEletronicos, int refeicoesComCarne)
+  {
+    return (quilometrosPorDia * 365 * 0.2) + (horasDeEletronicos * 0.1) + (refeicoesComCarne * 0.5);
+  }
+
+}
