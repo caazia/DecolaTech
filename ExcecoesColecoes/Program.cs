fa6fb2234d9295b07a73476f0550feabd272bb50
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-12-06 18:47:01 -0300

    Add files via upload

---
+++ ExcecoesColecoes/Program.cs
@@ -0,0 +1,105 @@
+﻿using ExcecoesColecoes.Models;
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("PE", "Pernambuco");
+estados.Add("MG", "Minas Gerais");
+
+string chave = "BA2";
+Console.WriteLine($"Verificando o elemento: {chave}");
+
+if(estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+ }
+ else{
+     Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+ }
+
+Console.WriteLine(estados["MG"]);
+
+
+// foreach(var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
+
+// Console.WriteLine("--------------------");
+
+// estados.Remove("SP");
+// estados["MG"] = "Minas Gerais - valor alterado";
+
+// foreach(var item in estados)
+// {
+//     Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+// }
+
+
+
+// Stack<int> pilha = new Stack<int>();
+
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+// pilha.Push(10);
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+// pilha.Push(20);
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+//new ExemploExcecao().Metodo1();
+
+
+
+// try{
+// string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+// foreach(string linha in linhas)
+// {
+//     Console.WriteLine(linha);
+// }
+    
+// } catch(FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// } catch(Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
+// finally
+// {
+//     Console.WriteLine("Realizou todo o comando.")
+// }
+
