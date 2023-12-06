ed3de73fdd307e4c587b3d1c2dbe07be259b581b
Cássia Carvalho <110124751+caazia@users.noreply.github.com>
2023-12-05 23:28:46 -0300

    Add files via upload

---
+++ ManipulandoValores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+string dataString = "2023-10-15 15:00";
+
+DateTime.TryParseExact
+(
+    dataString, 
+    "yyyy-MM-dd HH:mm",
+    CultureInfo.InvariantCulture,
+    DateTimeStyles.None,
+    out DateTime data
+);
+
+Console.WriteLine(data);
+
+
+
+// DateTime data = DateTime.Now;
+
+// Console.WriteLine(data.ToShortTimeString());
+
+// Console.WriteLine(data.ToString("dd/MM/yy HH:mm"));
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+// decimal valorMonetario = 1582.40M;
+
+// Console.WriteLine(valorMonetario.ToString("C5"));
+
+// double porcentagem = .3421;
+
+// Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 123456;
+// Console.WriteLine(numero.ToString("##-##-##"));
+
+
+
+
+// string numero1 = "10";
+// string numero2 = "20";
+
+// string resultado = numero1 + numero2;
+
+// Console.WriteLine(resultado);
